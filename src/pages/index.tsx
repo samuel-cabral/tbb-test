18337b0dde0234330e0767e66e3ac07915f381fd
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-08-03 22:39:00 -0300

    create home page with products catalog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,54 @@
+import { Flex, SimpleGrid } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+
+import { Product } from '../../typings';
+import { Header } from '../components/Header';
+import { ProductCard } from '../components/ProductCard';
+
 export default function Home() {
-  return <div>Home!</div>;
+  const [products, setProducts] = useState<Product[]>([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3333/data')
+      .then(response => response.json())
+      .then(data => setProducts(data.nodes));
+  }, []);
+
+  const filteredProducts =
+    search.length > 0
+      ? products.filter(product =>
+          new RegExp(search, 'i').test(product.category.name),
+        )
+      : [];
+
+  return (
+    <Flex direction="column" h="100vh" align="center" mt="5">
+      <Header search={search} setSearch={setSearch} />
+      <Flex
+        direction="column"
+        w="100%"
+        my="28"
+        maxWidth={1480}
+        mx="auto"
+        px="4"
+        pb="8"
+        gap="8"
+      >
+        {search.length > 0 ? (
+          <SimpleGrid flex="1" gap="4" minChildWidth="320px">
+            {filteredProducts.map(product => (
+              <ProductCard key={product.id} data={product} />
+            ))}
+          </SimpleGrid>
+        ) : (
+          <SimpleGrid flex="1" gap="4" minChildWidth="320px">
+            {products.map(product => (
+              <ProductCard key={product.id} data={product} />
+            ))}
+          </SimpleGrid>
+        )}
+      </Flex>
+    </Flex>
+  );
 }
